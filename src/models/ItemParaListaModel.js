996f59c35f0d94e99356bcd891475ac56d7ca9c3
Ruan <ruanalmeidawegner@gmail.com>
2022-08-16 17:39:05 -0300

    criado model dos itens das listas

---
+++ src/models/ItemParaListaModel.js
@@ -0,0 +1,24 @@
+const Sequelize = require ("sequelize");
+const conexao = require("../../database/database");
+const listaDeCompraModel = require("./ListaDeCompraModel");
+const UsuarioModel = require("./UsuarioModel");
+
+const ItemParaListaModel = conexao.define('itens',{
+    idItem:{
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement:true
+    },
+    nomeItem:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+
+});
+
+
+ItemParaListaModel.belongsTo(listaDeCompraModel); //um item tem uma lista de compras.
+listaDeCompraModel.hasMany(ItemParaListaModel); //uma lista de compras tem vários itens.
+
+ItemParaListaModel.sync({force:false});
+module.exports = ItemParaListaModel;
